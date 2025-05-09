94090594945b1d50c82844f76daf09b1dfdf19ba
romirkul <ommirkulr@gmail.com>
2025-05-09 15:59:51 +0500

    Finshed with category

--- internal/core/service/catergory.go
+++ internal/core/service/catergory.go
@@ -4,22 +4,34 @@ import (
 	"category/internal/core/domain"
 	"category/internal/core/port"
 	"context"
+	"fmt"
+	"strconv"
 )
 
 type CategoryService struct {
-	client *port.CategoryClient
+	client port.CategoryClient
 }
 
-func NewCategoryService(client *port.CategoryClient) *CategoryService {
+func NewCategoryService(client port.CategoryClient) *CategoryService {
 	return &CategoryService{
 		client,
 	}
 }
 
 func (s *CategoryService) ListCategories(ctx context.Context, params domain.ListParamsSt, ids []int64) (*domain.CategoryListRep, error) {
-	return nil, nil
+	stringIds := make([]string, len(ids))
+	for i, id := range ids {
+		stringIds[i] = strconv.FormatInt(id, 10)
+	}
+
+	return s.client.ListCategories(context.Background(), params, stringIds)
 }
 
 func (s *CategoryService) GetCategory(ctx context.Context, id int64) (*domain.CategoryMain, error) {
-	return nil, nil
+	if id < 0 {
+		return nil, fmt.Errorf("id cannot be empty")
+	}
+	stringId := strconv.FormatInt(id, 10)
+
+	return s.client.GetCategory(context.Background(), stringId)
 }
